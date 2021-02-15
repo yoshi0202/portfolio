98144ce1e1b2954f0dd6a93b673229853f2129ea
yoshi0202 <kobayashi020202@gmail.com>
2021-02-15 16:27:41 +0900

    fix: mail address & slide up position

--- js/main.js
+++ js/main.js
@@ -1,14 +1,12 @@
-jQuery(document).ready(function( $ ) {
-
-  $(window).scroll(function() {
-    $('.si').each(function() {
+jQuery(document).ready(function ($) {
+  $(window).scroll(function () {
+    $(".si").each(function () {
       var imagePos = $(this).offset().top;
 
       var topOfWindow = $(window).scrollTop();
-      if (imagePos < topOfWindow + 400) {
+      if (imagePos < topOfWindow + 700) {
         $(this).addClass("slideUp");
       }
     });
   });
-
 });
